073c0556230422d8e64056a8b3899f09dc4df26b
Evan Thibodeau <evan.thibodeau@liferay.com>
2025-12-13 18:01:28 +0400

    Send an Email with Resend

--- app/api/billing/route.tsx
+++ app/api/billing/route.tsx
@@ -1,3 +1,22 @@
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 export async function POST() {
-  return Response.json({success: 'Hello World'});
+  try {
+    const { data, error } = await resend.emails.send({
+      from: 'Acme <onboarding@resend.dev>',
+      to: ['delivered@resend.dev'],
+      subject: 'Billing Failure',
+      html: '<h1>Your payment did not go through.</h1>',
+    });
+
+    if (error) {
+      return Response.json({ error }, { status: 500 });
+    }
+
+    return Response.json({ data });
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
 }
